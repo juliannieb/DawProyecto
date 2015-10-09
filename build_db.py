6faecc433c9570b2f43edb7b947e672bea47fd9b
csssaz <alecassis16@gmail.com>
2015-10-09 17:13:20 -0500

    gitignore updated

--- build_db.py
+++ build_db.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, UnicodeText
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
