ff75ac8210313fa6491af881b6fb1326f9360d01
csssaz <alecassis16@gmail.com>
2015-10-11 17:22:31 -0500

    proper virtualenv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/alejandrocassis/Documents/daw_proyecto/venv"
+set -gx VIRTUAL_ENV "/Users/alejandrocassis/Documents/daw_proyecto/DawProyecto/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
